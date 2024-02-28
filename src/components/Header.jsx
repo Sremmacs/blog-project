37ae34164c651933e35c7442aa2cd8343a0a8bd2
Sremmacs <154491946+Sremmacs@users.noreply.github.com>
2024-02-28 07:28:06 +0000

    3 - Creating Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import { images } from '../constants/';
 
+
+const NavItemsInfo =[
+   {name:"Home"},
+   {name:"Articles"},
+   {name:"Pages"},
+   {name:"Pricing"},
+   {name:"Faq"},
+];
+
+
+const NavItem = ({name}) => {
+return (
+    <li className='relative group'>
+<a href='/' className='px-4 py-2'>{name}</a>
+<span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+  /
+  </span>
+</li>
+ );
+}
+
 const Header = () => {
   return  <section>
-    <header className='container mx-auto px-5 flex justify-between'>
+    <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
       <div>
         <img src={images.Logo} alt="logo" />
       </div>
-      <div>
-        <ul>
-          <li>
-             <a href='/'>Home</a>
-          </li>
-          <li>
-             <a href='/'>Articles</a>
-          </li>
-          <li>
-             <a href='/'>Pages</a>
-          </li>
-          <li>
-             <a href='/'>Pricing</a>
-          </li>
-          <li>
-             <a href='/'>Faq</a>
-          </li>
+      <div className='flex gap-x-9 items-center'>
+        <ul className='flex gap-x-5 font-semibold'>
+        {NavItemsInfo.map((items) => (
+          <NavItem key={items.name} name={items.name} />
+        ))}
+          
         </ul>
-        <button>Sign in</button>
+        <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
       </div>
     </header>
   </section>;
