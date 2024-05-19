803a1e407abc8eba651f74bd920bd46efaeeda64
Sremmacs <154491946+Sremmacs@users.noreply.github.com>
2024-05-19 16:39:33 +0000

    24 - Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import {Toaster} from 'react-hot-toast'
 
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
-import {Toaster} from 'react-hot-toast'
-
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Toaster />
     </div>
